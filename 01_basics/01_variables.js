264645da4c7f333ad931c6399a5ecfa5ccf9a3aa
n1meshh <nimeshadhikari1216@gmail.com>
2025-06-28 16:11:32 +0545

    firstCommit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 11211 //const ekchoti declare garisake paxi change hudeina
+let accountEmail = "nimeshadhikari@google.com"
+let accountState;  //value ma kei diyena vane undefined.
+
+// var accountPassword = "123" var use nagarne scope ko karand le                
+
+
+accountEmail = "nimesh@12"
+accountPassword = "1231"
+accountCity = "ktm"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version
+// alert( 3+3 ) we are using node js , not browswer 
+
+  //premitive data-types
+//number 3,4,5...
+//bigint thulo value
+//sting "nimesh"
+//boolean =>true/false
+//null => standalone value =>null object ho
+//undefeined=> yesko type undefined ho.
+
+//object
+
+console.log(typeof "hitesh")
